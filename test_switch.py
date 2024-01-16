306ba217ba26c3810bd515e431f75dc6460bb555
Hamza Daoud <hamzadaoud99@gmail.com>
2024-01-16 20:06:20 +0300

    test buttons

--- test_switch.py
+++ test_switch.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     print(f'15: {GPIO.input(15)}')
     print(f'16: {GPIO.input(16)}')
+    time.sleep(0.5)
