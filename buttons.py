a2a4afa2747bec52f58c61603d762c56359e27d5
Hamza Daoud <hamzadaoud99@gmail.com>
2024-01-16 19:57:54 +0300

    buttons

--- buttons.py
+++ buttons.py
@@ -4,21 +4,20 @@ import telegram_sender
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-leftSwitch = GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-rightSwitch = GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-gearSwitch = GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-redLed = GPIO.setup(29, GPIO.OUT)
-buzzer = GPIO.setup(36, GPIO.OUT)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(29, GPIO.OUT)
+GPIO.setup(36, GPIO.OUT)
 start_time = time.time()
-gear_time = 5
+diff_allowed = 5
 try:
     while True:
         if (GPIO.input(15) == GPIO.LOW) or (GPIO.input(16) == GPIO.LOW):
             current_time = time.time()
             diff = int(current_time - start_time)
             print(f"One of steering buttons is not pressed for {diff} seconds")
-            if diff >= gear_time or ((GPIO.input(15) == GPIO.LOW) and (GPIO.input(16) == GPIO.LOW)):
-                if diff % 10 == 0 or diff == gear_time:
+            if diff >= diff_allowed or ((GPIO.input(15) == GPIO.LOW) and (GPIO.input(16) == GPIO.LOW)):
+                if diff % 10 == 0:
                     telegram_sender.sendMessage("The driver is not driving safely!")
                 GPIO.output(29, 1)
                 GPIO.output(36, 1)
